d05008d8cd67303b99fa2fd088a6bc2311d52519
José González <josepsebastian171@gmail.com>
2018-12-25 17:38:13 -0500

    Solución inicial

---
+++ matriz/matriz/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos ha crear una matriz N*M");
+        }
+    }
+}
